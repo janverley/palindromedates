72109f52cbba803dacbeb7355221cdd96f341c38
Jan Verley <janverley@gmail.com>
2021-02-12 22:06:11 +0100

    palindrome dates tot ik 100 ben

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,31 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var d = DateTime.Now;
+            var c = 1;
+            do
+            {
+                if (IsPalindromeDate(d))
+                {
+                    Console.WriteLine(c++);
+                    Console.WriteLine(d.ToShortDateString().Replace("/", ""));
+                }
+
+                d = d.AddDays(1);
+            } while (d < new DateTime(2074, 1, 1));
+        }
+
+        private static bool IsPalindromeDate(DateTime dateTime)
+        {
+            var s = dateTime.ToString("ddMMyyyy");
+            return s[0] == s[7] &&
+                   s[1] == s[6] &&
+                   s[2] == s[5] &&
+                   s[3] == s[4];
         }
     }
 }
